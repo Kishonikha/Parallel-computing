125933bcdf7d16b6737345f5240bacd7fc57ef24
Kishonikha <146628890+Kishonikha@users.noreply.github.com>
2025-01-19 11:02:29 +0530

    Add files via upload

--- FileName.cpp
+++ FileName.cpp
@@ -1,21 +1,23 @@
 #include <mpi.h>
-#include <stdio.h>
+#include <stdio.h>         
+
 int main() {
 	int n;
 	int pid;
 	MPI_Init(NULL, NULL);
 	MPI_Comm_size(MPI_COMM_WORLD, &n);
 	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+	MPI_Status sta;
 
 	if (pid == 0) {
-		printf("I am the master .my pid is %d \n", pid);
-	}
-	else if(pid==1) {
-		printf("I am the second master.my pid is %d \n", pid);
+		int msg = 2;
+		for (int i = 1; i < n; i++) {
+			MPI_Send(&msg, 1, MPI_INT, i, 50, MPI_COMM_WORLD);
+		}
+		printf("My processor id is %d,Im the sender,The msg is %d \n", pid, msg);
 	}
 	else {
-		printf("I am the slave.my pid is %d \n", pid);
+		int Rec_msg;
+		MPI_Recv(&Rec_msg, 1, MPI_INT, 0, 50, MPI_COMM_WORLD, &sta);
 	}
-	MPI_Finalize();
-	return 0;
 }
